c330ac17cc6a75ce89a622dff84e30721cfa1ce6
ldk7811 <ldk7811@rit.edu>
2023-10-14 14:30:17 -0400

    Removed calls. Added wrapper, and basic function

---
+++ src/main/java/Comic/ComicSlabbedDecorator.java
@@ -0,0 +1,13 @@
+package Comic ;
+
+public class ComicSlabbedDecorator extends BaseComicDecorator{
+    private final IComic wrapped;
+    public ComicSlabbedDecorator(IComic wrapped){
+        super(wrapped);
+        this.wrapped = wrapped;
+    }
+
+    public double getValue() {
+        return wrapped.getValue()*2;
+    }
+}
