b06c7c9ff0a2df1610aa9d189bcafa6d577546dd
ldk7811 <ldk7811@rit.edu>
2023-10-14 14:29:01 -0400

    Added abstract decorator

---
+++ src/main/java/Comic/BaseComicDecorator.java
@@ -0,0 +1,24 @@
+package Comic ;
+
+public abstract class BaseComicDecorator implements IComic{
+    private final IComic wrapped;
+    private double value;
+
+    BaseComicDecorator(IComic wrapped, double value) {
+        this.wrapped = wrapped;
+        this.value = value;
+    }
+
+    BaseComicDecorator(IComic wrapped) {
+        this(wrapped, 5) ;
+    }
+
+    @Override
+    public double getValue() {
+        return value;
+    }
+
+    public String getPublisher() {
+        return wrapped.getPublisher();
+    }
+}
