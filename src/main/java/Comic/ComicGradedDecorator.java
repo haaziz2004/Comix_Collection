1b1f8b90a7243412b0e2103a5ed4270e9bb547c4
ldk7811 <ldk7811@rit.edu>
2023-10-14 14:29:51 -0400

    Removed calls. Added wrapper, and stored it

---
+++ src/main/java/Comic/ComicGradedDecorator.java
@@ -0,0 +1,13 @@
+package Comic ;
+
+public class ComicGradedDecorator extends BaseComicDecorator{
+    private final IComic wrapped;
+    public ComicGradedDecorator(IComic wrapped){
+        super(wrapped);
+        this.wrapped = wrapped;
+    }
+
+    public double getValue() {
+        return wrapped.getValue()*3;
+    }
+}
